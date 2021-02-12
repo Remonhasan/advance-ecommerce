f0e0b931690d25a56120efd15dbe0d3e1f017507
remonhasan <remonhasan041@gmail.com>
2021-02-12 19:45:52 +0600

    added admin routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('/admin')->namespace('Admin')->group(function(){
+  // admin routes
+    Route::match(['get','post'],'/','AdminController@login');
+    Route::group(['middleware'=>['admin']],function(){
+        Route::get('dashboard','AdminController@dashboard');
+    });
+
+
+});
+
