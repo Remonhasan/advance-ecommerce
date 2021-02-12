4ddb28f3248610fe07b4c60d6904dd57a945cc12
remonhasan <remonhasan041@gmail.com>
2021-02-12 18:03:49 +0600

    first commit

---
+++ resources/views/layouts/admin_layout/admin_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2021 <a href="https://github.com/Remonhasan">remon hasan</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>University</b> uap
+    </div>
+  </footer>
