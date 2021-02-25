d49f08112fc9d4c2be121c3ecce80802cd0ea241
remonhasan <remonhasan041@gmail.com>
2021-02-25 20:24:56 +0600

    updated necessary requirements

--- resources/views/admin/categories/categories.blade.php
+++ resources/views/admin/categories/categories.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('/admin/dashboard') }}">Home</a></li>
                             <li class="breadcrumb-item active">Categories</li>
                         </ol>
                     </div>
@@ -24,6 +24,7 @@
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title">Categories</h3>
+                            <a href="{{ url('admin/add-edit-category') }}" style="max-width: 150px; float:right; display:inline-block;" class="btn btn-block btn-info">Add Category</a>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
